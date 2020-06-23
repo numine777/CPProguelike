d036ee0ee3bed724ca9c685b910a243e6ba6a3c4
Scott Walls <scottwalls2008@gmail.com>
2020-06-23 16:19:06 +0000

    2D Game Engine
    Initial commit adding functionality to render the initial window and game loop.
    
    Dependencies:
    SDL2

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include "game.h"
+
+Game *game = nullptr;
+
+int main() {
+
+    game = new Game();
+
+    game->init("WallsEngine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
+
+    while(game->running()) {
+
+        game->handleEvents();
+        game->update();
+        game->render();
+
+    }
+
+    game->clean();
+
+    return 0;
+} 
